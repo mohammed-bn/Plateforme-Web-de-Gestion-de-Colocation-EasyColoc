88c71c1463d4f901c474996a47a141b9fa3e3f30
mohammed-bn <mbentiss11@gmail.com>
2026-02-26 23:16:59 +0000

    created controller catygory, colocation, expenses, joins, payment.

---
+++ EasyColoc/app/Http/Controllers/ColocationController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Colocation;
+use App\Http\Requests\StoreColocationRequest;
+use App\Http\Requests\UpdateColocationRequest;
+
+class ColocationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreColocationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Colocation $colocation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Colocation $colocation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateColocationRequest $request, Colocation $colocation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Colocation $colocation)
+    {
+        //
+    }
+}
---
+++ EasyColoc/app/Http/Controllers/JoinsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Joins;
+use App\Http\Requests\StoreJoinsRequest;
+use App\Http\Requests\UpdateJoinsRequest;
+
+class JoinsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreJoinsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Joins $joins)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Joins $joins)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateJoinsRequest $request, Joins $joins)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Joins $joins)
+    {
+        //
+    }
+}
